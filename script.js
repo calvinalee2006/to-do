901ab112f4597a5291d05ee279b55ce194d5d43a
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-13 18:00:37 -0600

    item list

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const form = document.getElementById('formSubmit').addEventListener('click')
+const form = document.getElementById('formSubmit');
+const items = document.getElementById('listItems');
+const input = document.getElementById('action');
+const strike = document.getElementById('strike');
+
+form.addEventListener('submit', addItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+    const todo = input.value
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(todo));
+    items.appendChild(li);
+}
+
+items.addEventListener('submit', removeItems);
+
+function removeItems() {
+    const items = input.value
+    items.removeChild(items.childNodes[1]);
+}
+
+
+
+//create a list item
+//create a textnode
+//append the textnode to the list item
+//append the list item to ul. 
