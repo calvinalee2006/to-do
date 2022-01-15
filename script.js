24d6e4a1f78d14052db8af666f44c87753209712
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-14 21:16:44 -0600

    finished

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const form = document.getElementById('formSubmit');
 const items = document.getElementById('listItems');
 const input = document.getElementById('action');
 const strike = document.getElementById('strikeItem');
+const remove = document.getElementById('discardItem')
 
 form.addEventListener('submit', addItem);
+items.addEventListener('click', removeItem);
 
 function addItem(e) {
     e.preventDefault();
@@ -11,14 +13,29 @@ function addItem(e) {
     const todo = input.value
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(todo));
+
+    var removeBtn = document.createElement('button')
+    removeBtn.className = 'btn-remove';
+    removeBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(removeBtn)
+
     items.appendChild(li);
 }
+
 items.addEventListener("click", strikeItems);
 function strikeItems(e) {
     e.target.classList.toggle('completed')
-
-    // strikethrough functionality goes here
-    // HW: work on getting the todo being clicked on to have a strikethrough on it
 }
+
+function removeItem(e) {
+    if (e.target.classList.contains('btn-remove')) {
+        if (confirm('Are you sure?')) {
+            var li = e.target.parentElement;
+            items.removeChild(li);
+        }
+    }
+}
+
+
 
 
